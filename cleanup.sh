b948e985cf8482f45d3266b59e795ff58cfdc168
Qeas <alexey.khodos@nexenta.com>
2016-07-14 22:42:12 +0300

    clean snapshots and cover cinder iscsi/nfs and manila nfs

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,6 @@
-for list in `zfs list | grep "pool1/nfs_share/volume-" | awk '{print $1}' ` ; do zfs destroy $list ; done
+for list in `zfs list -t snapshot | grep "pool1/iscsi/" | awk '{print $1}'` ; do zfs destroy $list; done
+for list in `zfs list | grep "pool1/iscsi/" | awk '{print $1}'` ; do zfs destroy $list; done
+for list in `zfs list -t snapshot | grep "pool1/nfs_share/share" | awk '{print $1}'` ; do zfs destroy $list; done
+for list in `zfs list | grep "pool1/nfs_share/share-" | awk '{print $1}'` ; do zfs destroy $list; done
+for list in `zfs list -t snapshot | grep "pool1/nfs_share/volume-" | awk '{print $1}'` ; do zfs destroy $list; done
+for list in `zfs list | grep "pool1/nfs_share/volume-" | awk '{print $1}'` ; do zfs destroy $list; done 
