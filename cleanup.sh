1908dcfa1a6a082961db70fbe8bb0a72ecbc007e
Qeas <alexey.khodos@nexenta.com>
2016-10-14 23:15:29 +0300

    restart nms after cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,4 @@ for list in `zfs list -t snapshot | grep "pool1/nfs_share/share" | awk '{print $
 for list in `zfs list | grep "pool1/nfs_share/share-" | awk '{print $1}'` ; do zfs destroy $list; done
 for list in `zfs list -t snapshot | grep "pool1/nfs_share/volume-" | awk '{print $1}'` ; do zfs destroy $list; done
 for list in `zfs list | grep "pool1/nfs_share/volume-" | awk '{print $1}'` ; do zfs destroy $list; done 
+sudo svcadm restart nms
