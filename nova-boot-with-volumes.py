fc3b75d45cc8cb0c595b22789fb7fdefeb8e9a1f
Qeas <alexey.khodos@nexenta.com>
2017-01-13 16:25:11 +0300

    count as param in nova-boot, cores in quota-update

--- nova-boot-with-volumes.py
+++ nova-boot-with-volumes.py
@@ -1,15 +1,21 @@
+import argparse
 import os
 from novaclient import client as novacl
 import subprocess
-# from cinderclient import client as cindercl
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'amount', metavar='N', type=int,
+    help='Amount of nova instances to boot')
+args = parser.parse_args()
 
 nova = novacl.Client(
     '2', os.environ['OS_USERNAME'], os.environ['OS_PASSWORD'],
     os.environ['OS_TENANT_NAME'], os.environ['OS_AUTH_URL'])
 fl = nova.flavors.find(name='m1.nano')
 im = nova.images.find(name='cirros-0.3.4-x86_64-uec')
-for i in range(10):
+for i in range(args.amount):
     subprocess.call([
         'nova', 'boot', '--flavor', fl.id, '--block-device',
         'source=image,id=%s,dest=volume,size=1,shutdown=remove,bootindex=0' % (
--- quota_update.py
+++ quota_update.py
@@ -9,4 +9,6 @@ for i in range(len(s.strip().split('|\n|'))):
     pr_id = s.strip().split('|\n|')[i].split('|')[0].strip()
     subprocess.call([
         'cinder', 'quota-update', '--gigabytes', val, '--volumes',
-        val, '--snapshots', val, '202afef5ae7f4123a9fc0b1dd685a89f'])
+        val, '--snapshots', val, pr_id])
+    subprocess.call([
+        'nova', 'quota-update', '--instances', val, 'cores', val, pr_id])
